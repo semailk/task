880a7cd639180b9f12aeaf71dccf595756ddc1fd
Semailk <sem.alik@mail.ru>
2021-02-01 10:20:34 +0300

    table characters

---
+++ database/migrations/2021_01_31_100923_create_characters_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCharactersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('characters', function (Blueprint $table) {
+            $table->id();
+            $table->integer('episode_id');
+            $table->string('name');
+            $table->date('birthday');
+            $table->json('occupations');
+            $table->string('img')->nullable();
+            $table->string('nickname');
+            $table->string('portrayed');
+            $table->timestamps();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('characters');
+    }
+}
