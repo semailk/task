e37487b70ec1f3ecb1d2c7115e66aa0488a8e8a0
Semailk <sem.alik@mail.ru>
2021-02-01 10:22:20 +0300

    creating test data in episodes table

---
+++ database/factories/EpisodeFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Episode;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class EpisodeFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Episode::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'title' => $this->faker->title,
+            'air_date' => $this->faker->date()
+        ];
+    }
+}
---
+++ database/seeders/EpisodeSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Episode;
+use Illuminate\Database\Seeder;
+
+class EpisodeSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        for ($i=1; $i <=30; $i++){
+            Episode::factory()->create();
+        }
+    }
+}
